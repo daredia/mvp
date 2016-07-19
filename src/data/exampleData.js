cfd1e13b18d5ea54057cc0b011247003d3ab9205
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-18 17:58:41 -0700

    Update example data

--- src/data/exampleData.js
+++ src/data/exampleData.js
@@ -1,5 +1,5 @@
-window.exampleProfileData = [{
-  image: '',
+window.exampleProfileData = [{ // array of profile objects
+  image: 'http://factmag-images.s3.amazonaws.com/wp-content/uploads/2013/12/beyonce-121313.jpg',
   name: 'Beyonce',
   age: 29,
   distance: 10,
@@ -8,26 +8,26 @@ window.exampleProfileData = [{
     occupation: 'Musician',
     text: 'I am fierce, and I dont think you ready for this jelly'
   }
-}]; // array of profile objects
+}]; 
 
-window.exampleMessageData = [{
+window.exampleMessageData = [{ // array of message objects
   name: 'Kanye',
   age: 31,
-  image: '',
+  image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
   messages: ['I made that bitch famous'],
   new: false
 },
 {
   name: 'Kanye',
   age: 31,
-  image: '',
+  image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
   messages: ['I made that bitch famous'],
   new: true
 },
 {
   name: 'Kanye',
   age: 31,
-  image: '',
+  image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
   messages: ['I made that bitch famous'],
   new: false
-}]; // array of message objects
+}]; 
