1e0b382752e700c7271dd9c667e42ac0972feb01
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-19 15:29:01 -0700

    Continue rendering sample data in components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,10 @@
 var Profile = ({profile}) => (
   <div>
     <img className="responsive-img" src={`${profile.image}`}/>
+    <p>{profile.name}, {profile.age}</p>
+    <p>{profile.distance} miles away</p>
+    <a className="waves-effect waves-light btn-large"><i className="material-icons left">not_interested</i>no</a>
+    <a className="waves-effect waves-light btn-large"><i className="material-icons left">done</i>yes</a>
   </div>
 );
 
