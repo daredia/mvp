5e841f26eeffd7930b15ccc32c0d1442af877113
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-20 00:27:48 -0700

    Continue styling components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,11 @@
 var Profile = ({profile}) => (
   <div>
     <img className="responsive-img" src={`${profile.image}`}/>
-    <p>{profile.name}, {profile.age}</p>
-    <p>{profile.distance} miles away</p>
-    <a className="waves-effect waves-light btn-large"><i className="material-icons left">not_interested</i>no</a>
-    <a className="waves-effect waves-light btn-large"><i className="material-icons left">done</i>yes</a>
+    <span className="left">{profile.name}, {profile.age}</span>
+    <span className="right">{profile.distance} miles away</span>
+    <hr className="separator"/>
+    <a className="left waves-effect waves-light btn-large red"><i className="material-icons left">not_interested</i>no</a>
+    <a className="right waves-effect waves-light btn-large"><i className="material-icons left">done</i>yes</a>
   </div>
 );
 
