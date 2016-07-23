ef0b322f1eafa8d0aafb617e8f64c29eaa582a6e
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-22 17:55:18 -0700

    Fix App's layout using flexboxgrid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,14 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 var App = () => (
   <MuiThemeProvider>
-    <div className="row center-lg">
-      <div className="col-lg-5">
-        <Profile profile={exampleProfileData[0]}/>
-      </div>
-      <div className="col-lg-7">
-        <MessageList messages={exampleMessageData}/>
+    <div className="row center-xs">
+      <div className="row col-xs-8">
+        <div className="start-xs col-xs-8">
+          <Profile profile={exampleProfileData[0]}/>
+        </div>
+        <div className="start-xs col-xs-4">
+          <MessageList messages={exampleMessageData}/>
+        </div>
       </div>
     </div>
   </MuiThemeProvider>
