e38e107a1bb4df0d5ba1e79705cb1bcc160edd32
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-25 22:47:51 -0700

    Make MessageList component dynamically render list of messages based on sample data

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -7,36 +7,40 @@ import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 
-var MessageList = ({messages}) => (
-  <List>
-    <Subheader>Messages: 28, 7 new</Subheader> {/* TODO: make dynamic */}
-    <ListItem
-      leftAvatar={<Avatar className="avatar" src={messages[0].image} />}
-      /*primaryText="Brunch this weekend?"*/
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>{messages[0].name}</span> --
-          {' ' + messages[0].messages[0]}
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-    <Divider inset={true} />
-    <ListItem
-      leftAvatar={<Avatar className="avatar" src={messages[0].image} />}
-      /*primaryText={
-        <p className="messageTitle">Summer BBQ&nbsp;&nbsp;<span style={{color: lightBlack}}>4</span></p>
-      }*/
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>{messages[0].name}</span> --
-          {' ' + messages[1].messages[0]}
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-    <Divider inset={true} />
-  </List>
-);
+var MessageList = ({messages}) => {
+  var newMessageCount = messages.filter(function(message) {
+    return message.new;
+  }).length;
+  var newMessageString = newMessageCount + ' new message';
+  newMessageString = newMessageCount > 1 ? newMessageString + 's' : newMessageString;
+
+  return (
+    <List>
+      <Subheader style={{color: darkBlack}}>
+        {newMessageString} | {messages.length} total
+      </Subheader> 
+      
+      {messages.map(function(message) {
+        return (
+          <div key={message.id}>
+            <ListItem
+              leftAvatar={<Avatar className="avatar" src={message.image} />}
+              /*primaryText="Brunch this weekend?"*/
+              secondaryText={
+                <p>
+                  <span style={{color: darkBlack}}>{message.name}</span> --
+                  {' ' + message.messages[0]}
+                </p>
+              }
+              secondaryTextLines={2}
+            />
+            <Divider inset={true} />
+          </div>
+        );
+      })}
+      
+    </List>
+  );
+};
 
 export default MessageList;
--- src/data/exampleData.js
+++ src/data/exampleData.js
@@ -13,24 +13,27 @@ var exampleProfileData = [{ // array of profile objects
 }]; 
 
 var exampleMessageData = [{ // array of message objects
+  id: 1,
   name: 'Kanye',
   age: 31,
   image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
   messages: ['I will be in your neighborhood doing errands this weekend. Do you want to grab brunch?'],
-  new: false
-},
-{
-  name: 'Kanye',
-  age: 31,
-  image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
-  messages: ['I made that bitch famous'],
   new: true
 },
 {
+  id: 2,
   name: 'Kanye',
   age: 31,
   image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
   messages: ['I made that bitch famous'],
+  new: false
+},
+{
+  id: 3,
+  name: 'Kanye',
+  age: 31,
+  image: 'http://imf.ulximg.com/image/300x300/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/8836f2626a5fce620c3db02a6108439f/1392767353_kanye_west_wall_40.jpg',
+  messages: ['Ima let you finish but...'],
   new: false
 }]; 
 
